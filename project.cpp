e1d661c6844e2b59f23d836126d55f6cbf07fdb1
Muhammad Talha <104964352+MuhmmadTalha@users.noreply.github.com>
2024-11-04 02:21:08 +0500

    fixed some bugs and added score batting and bowling system (next update will be about player gets out or over is finished)

--- project.cpp
+++ project.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <string>
-#include <vector>
 #include <cstdlib>
 #include <ctime>
 using namespace std;
@@ -28,13 +27,15 @@ public:
 
 class Choices : public Player
 {
-private:
+protected:
     char choice;
     int outcome;
     string firstmover;
     string secondmover;
-    string onfield;
-    string offfield;
+    string tossW;
+    string tossL;
+    string bater;
+    string baller;
     int bb;
 
 public:
@@ -92,16 +93,16 @@ public:
     {
         if ((choice == 'H' || choice == 'h') && outcome == 1 || (choice == 'T' || choice == 't') && outcome == 0)
         {
-            onfield = firstmover;
-            offfield = secondmover;
+            tossW = firstmover;
+            tossL = secondmover;
             cout << "Congratulations! " << firstmover << ", you have won the toss." << endl;
             cout << "What would you choose, batting(1) or bowling(2)? : ";
             cin >> bb;
         }
         else
         {
-            onfield = secondmover;
-            offfield = firstmover;
+            tossW = secondmover;
+            tossL = firstmover;
             cout << "Sorry, " << firstmover << ", you have lost the toss." << endl;
             cout << secondmover << ", what would you choose, batting(1) or bowling(2)? : ";
             cin >> bb;
@@ -112,15 +113,85 @@ public:
     {
         if (bb == 1)
         {
-            cout << onfield << " has decided to bat! " << offfield << " will now bowl." << endl;
+            cout << tossW << " has decided to bat! " << tossL << " will now bowl." << endl;
+            bater = tossW;
+            baller = tossL;
         }
         else if (bb == 2)
         {
-            cout << onfield << " has decided to bowl! " << offfield << " will now bat." << endl;
+            cout << tossW << " has decided to bowl! " << tossL << " will now bat." << endl;
+            bater = tossL;
+            baller = tossW;
         }
         else
         {
-            cout << "Invalid choice for batting or bowling." << endl;
+            cout << "Invalid choice for batting or bowling. Please enter 1 for batting or 2 for bowling." << endl;
+            tossResult();
+            chooseRole();
+        }
+    }
+
+    string getBatter() const { return bater; }
+    string getBowler() const { return baller; }
+};
+
+class Gameplay : public Choices
+{
+private:
+    int baterMove;
+    int ballerMove;
+    int score = 0;
+
+public:
+    Gameplay(string n1, string n2, char c) : Choices(n1, n2, c) {}
+
+    void moveSelection()
+    {
+        for (int i = 0; i < 7; i++)
+        {
+            cout << "." << endl;
+        }
+
+        for (int i = 6; i >= 0; i--)
+        {
+            cout << getBatter() << " choose your run(0-6)!: ";
+            cin >> baterMove;
+
+            for (int j = 0; j < 3; j++)
+            {
+                cout << "." << endl;
+            }
+            cout << getBowler() << " choose your move(0-6)!: ";
+            cin >> ballerMove;
+
+            cout << endl
+                 << "Matching Result!..";
+            for (int k = 0; k < 3; k++)
+            {
+                cout << "." << endl;
+            }
+            moveChecker();
+
+            cout << endl
+                 << "Remaining balls: " << i << endl;
+        }
+    }
+
+    void moveChecker()
+    {
+        if (baterMove == ballerMove)
+        {
+            cout << getBatter() << " is OUT! with a score of " << score << ". Now " << getBowler() << "'s turn to bat." << endl;
+            // Reset or swap roles for the next innings can be handled here
+        }
+        else if (baterMove >= 0 && baterMove <= 6)
+        {
+            score += baterMove;
+            cout << "Nice shot! " << getBatter() << " scored " << baterMove << " runs, total score: " << score << endl;
+        }
+        else
+        {
+            cout << "Invalid move by " << getBatter() << ". Please choose a run between 0 and 6." << endl;
         }
     }
 };
@@ -128,15 +199,14 @@ public:
 int main()
 {
     srand(time(0));
-    cout << "Hello World" << endl;
+    cout << "Welcome to the Cricket Game Simulation!" << endl;
 
-    Player player("", "");
-    player.setNames();
-
-    Choices choice(player.getName1(), player.getName2(), ' ');
-    choice.selectChoices();
-    choice.tossResult();
-    choice.chooseRole();
+    Gameplay game("", "", ' ');
+    game.setNames();
+    game.selectChoices();
+    game.tossResult();
+    game.chooseRole();
+    game.moveSelection();
 
     return 0;
 }
